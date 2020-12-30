9ece9156b427b5752049b225f06149cfeb5a50f8
Eiren Rain <eirenliel@users.noreply.github.com>
2020-12-30 21:37:00 +0300

    Added calibration firmware and Magneto util, calibrate sensor for my MPU

---
+++ Magneto/stdafx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <math.h>
+#include <malloc.h>
+#include <string.h>
+#include <float.h>
+
+
+
+// TODO: reference additional headers your program requires here
