8faac586f0d744ef4d41daf2dcf8a30c10333438
ThreadOfFate <the__guy@hotmail.co.uk>
2022-01-23 17:00:36 +0000

    Fixed 1 imu issue.

--- src/main.cpp
+++ src/main.cpp
@@ -154,7 +154,7 @@ void setup()
         }
     #else
     uint8_t addr =I2CSCAN::pickDevice(ICM_ADDR_1, ICM_ADDR_2, true);
-    sensor.setupICM20948(true ? addr == ICM_ADDR_2 : false, addr);
+    sensor.setupICM20948(addr == ICM_ADDR_2, addr);
     #endif
 #endif
 
