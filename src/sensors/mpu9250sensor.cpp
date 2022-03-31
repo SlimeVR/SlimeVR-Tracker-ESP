148b917d5f829f1c85117d984681b55083f2ffbd
Eiren Rain <Eirenliel@users.noreply.github.com>
2022-03-31 16:54:22 +0300

    Update src/sensors/mpu9250sensor.cpp
    Co-authored-by: TheDevMinerTV <tobigames200@gmail.com>

--- src/sensors/mpu9250sensor.cpp
+++ src/sensors/mpu9250sensor.cpp
@@ -1,6 +1,6 @@
 /*
     SlimeVR Code is placed under the MIT license
-    Copyright (c) 2022 Eiren Rain, S.J. Remington, SlimeVR contributors
+    Copyright (c) 2021 Eiren Rain, S.J. Remington, SlimeVR contributors
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
     in the Software without restriction, including without limitation the rights
