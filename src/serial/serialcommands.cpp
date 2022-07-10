d132c2e4c6b04c69a4afc61a30f5f83fa99c5aa8
lucas lelievre <loucass003@gmail.com>
2022-07-11 00:27:24 +0200

    Add GET CONFIG serial command (#177)

--- src/serial/serialcommands.cpp
+++ src/serial/serialcommands.cpp
@@ -26,6 +26,7 @@
 #include "logging/Logger.h"
 #include <CmdCallback.hpp>
 #include "GlobalVars.h"
+#include "batterymonitor.h"
 
 #if ESP32
     #include "nvs_flash.h"
@@ -53,7 +54,11 @@ namespace SerialCommands {
     }
 
     void cmdGet(CmdParser * parser) {
-        if(parser->getParamCount() != 1 && parser->equalCmdParam(1, "INFO")  ) {
+        if (parser->getParamCount() < 2) {
+            return;
+        }
+        
+        if (parser->equalCmdParam(1, "INFO")) {
             logger.info(
                 "SlimeVR Tracker, board: %d, hardware: %d, build: %d, firmware: %s, address: %s",
                 BOARD,
@@ -63,6 +68,46 @@ namespace SerialCommands {
                 WiFiNetwork::getAddress().toString().c_str()
             );
             // TODO Print sensors number and types
+        }
+
+        if (parser->equalCmdParam(1, "CONFIG")) {
+            String str =
+                "BOARD=%d\n" 
+                "IMU=%d\n"
+                "SECOND_IMU=%d\n"
+                "IMU_ROTATION=%f\n"
+                "SECOND_IMU_ROTATION=%f\n"
+                "BATTERY_MONITOR=%d\n"
+                "BATTERY_SHIELD_RESISTANCE=%d\n"
+                "BATTERY_SHIELD_R1=%d\n"
+                "BATTERY_SHIELD_R2=%d\n"
+                "PIN_IMU_SDA=%d\n"
+                "PIN_IMU_SCL=%d\n"
+                "PIN_IMU_INT=%d\n"
+                "PIN_IMU_INT_2=%d\n"
+                "PIN_BATTERY_LEVEL=%d\n"
+                "LED_PIN=%d\n"
+                "LED_INVERTED=%d\n";
+
+            Serial.printf(
+                str.c_str(),
+                BOARD,
+                IMU,
+                SECOND_IMU,
+                IMU_ROTATION,
+                SECOND_IMU_ROTATION,
+                BATTERY_MONITOR,
+                BATTERY_SHIELD_RESISTANCE,
+                BATTERY_SHIELD_R1,
+                BATTERY_SHIELD_R2,
+                PIN_IMU_SDA,
+                PIN_IMU_SCL,
+                PIN_IMU_INT,
+                PIN_IMU_INT_2,
+                PIN_BATTERY_LEVEL,
+                LED_PIN,
+                LED_INVERTED
+            );
         }
     }
 
