5932ce86b09f2145a7926cb8f4955d135efad043
Kitlith <kitlith@kitl.pw>
2022-10-05 14:11:50 -0700

    Restore original magneto license.
    It's MIT licensed, I'm not concerned.

--- lib/magneto/magneto1.4.h
+++ lib/magneto/magneto1.4.h
@@ -1,16 +1,31 @@
 #ifndef __MAGENTO_1_4__
 #define __MAGENTO_1_4__
 
+/* Copyright (C) 2013 www.sailboatinstruments.blogspot.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 // In https://github.com/jremington/MPU-9250-AHRS
 // magneto 1.4 magnetometer/accelerometer calibration code
 // from http://sailboatinstruments.blogspot.com/2011/08/improved-magnetometer-calibration.html
 // tested and works with Code::Blocks 10.05 through 20.03
-
-// Command line version slightly modified from original, sjames_remington@gmail.com
-// Now includes option to reject outliers in units of sigma, deviation of data vector length
-// from mean values of the sample. Suggest using 2 as the rejection criterion
-
-// comma separated ASCII input data file expected, three columns x, y, z
 
 void CalculateCalibration(float *buf, int sampleCount, float BAinv[4][3]);
 
