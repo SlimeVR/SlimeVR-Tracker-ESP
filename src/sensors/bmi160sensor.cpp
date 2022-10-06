773788d2926861340472776bf1756befa340c8bf
0forks <114709761+0forks@users.noreply.github.com>
2022-10-06 03:31:25 +0300

    Revert accidental log change

--- src/sensors/bmi160sensor.cpp
+++ src/sensors/bmi160sensor.cpp
@@ -113,12 +113,12 @@ void BMI160Sensor::motionSetup() {
     #endif
 
     if (!imu.testConnection()) {
-        m_Logger.fatal("Can't connect to BMI160 (0x%02x) at address 0x%02x", imu.getDeviceID(), addr);
+        m_Logger.fatal("Can't connect to BMI160 (reported device ID 0x%02x) at address 0x%02x", imu.getDeviceID(), addr);
         ledManager.pattern(50, 50, 200);
         return;
     }
 
-    m_Logger.info("Connected to BMI160 (0x%02x) at address 0x%02x", imu.getDeviceID(), addr);
+    m_Logger.info("Connected to BMI160 (reported device ID 0x%02x) at address 0x%02x", imu.getDeviceID(), addr);
 
     int16_t ax, ay, az;
     getRemappedAcceleration(&ax, &ay, &az);
