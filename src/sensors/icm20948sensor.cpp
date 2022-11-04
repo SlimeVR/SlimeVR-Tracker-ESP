f91a758161a8695d23a947e5c1dad16fc3df4cca
ThreadOfFate <37180802+ThreadOfFate@users.noreply.github.com>
2022-11-04 23:48:58 +0000

    Update src/sensors/icm20948sensor.cpp
    Co-authored-by: TheDevMinerTV <tobigames200@gmail.com>

--- src/sensors/icm20948sensor.cpp
+++ src/sensors/icm20948sensor.cpp
@@ -645,7 +645,7 @@ ICM_20948_Status_e ICM_20948::initializeDMP(void)
     ICM_20948_smplrt_t mySmplrt;
     //mySmplrt.g = 19; // ODR is computed as follows: 1.1 kHz/(1+GYRO_SMPLRT_DIV[7:0]). 19 = 55Hz. InvenSense Nucleo example uses 19 (0x13).
     //mySmplrt.a = 19; // ODR is computed as follows: 1.125 kHz/(1+ACCEL_SMPLRT_DIV[11:0]). 19 = 56.25Hz. InvenSense Nucleo example uses 19 (0x13).
-    mySmplrt.g = 4; // 225Hzh
+    mySmplrt.g = 4; // 225Hz
     mySmplrt.a = 4; // 225Hz
     // mySmplrt.g = 8; // 112Hz
     // mySmplrt.a = 8; // 112Hz
