d750c951f6998b9d1b64359b9cd4b62d14b0dd61
DevMiner <tobigames200@gmail.com>
2023-04-30 01:27:30 +0000

    impl: add a warning and env variable in the git commit grabber

--- scripts/get_git_commit.py
+++ scripts/get_git_commit.py
@@ -1,6 +1,11 @@
 import subprocess
+import os
 
 revision = "00000000000000000000000000000000000000000"
+
+env_rev = os.environ.get("GIT_REV")
+if env_rev != "":
+    revision = env_rev
 
 try:
     revision = (
@@ -8,7 +13,10 @@ try:
         .strip()
         .decode("utf-8")
     )
-except:
-    pass
+except Exception:
+    print("==========================================================================")
+    print("  WARNING: You are not in a Git repository or do not have Git installed.")
+    print("           The version string will be incorrect.")
+    print("==========================================================================")
 
 print(f"'-DGIT_REV=\"{revision}\"'")
