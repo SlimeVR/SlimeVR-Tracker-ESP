5827efe50c82f172c529adc43a9e71aacab4e0b1
sctanf <36978460+sctanf@users.noreply.github.com>
2023-05-15 15:46:59 -0500

    remove mahony/madgwick

--- lib/magneto/madgwick.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MADGWICK_H_
-#define _MADGWICK_H_
-
-#include "helper_3dmath.h"
-
-void madgwickQuaternionUpdate(float q[4], float ax, float ay, float az, float gx, float gy, float gz, float deltat);
-void madgwickQuaternionUpdate(float q[4], float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz, float deltat);
-
-#endif /* _MADGWICK_H_ */
