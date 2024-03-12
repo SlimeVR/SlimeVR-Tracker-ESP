juchong/SlimeVR-Tracker-ESP-BMI270-ICM-42688

Soft fusion sensor initial code, wip

src/configuration/CalibrationConfig.h
@@ -63,6 +63,23 @@ namespace SlimeVR {
             float temperature;
         };
 
+        struct SoftFusionCalibrationConfig {
+            // accelerometer offsets and correction matrix
+            float A_B[3];
+            float A_Ainv[3][3];
+
+            // magnetometer offsets and correction matrix
+            float M_B[3];
+            float M_Ainv[3][3];
+
+            // raw offsets, determined from gyro at rest
+            float G_off[3];
+
+            // calibration temperature for dynamic compensation
+            float temperature;
+        };
+
+
         struct MPU6050CalibrationConfig {
             // accelerometer offsets and correction matrix
             float A_B[3];
@@ -95,7 +112,7 @@ namespace SlimeVR {
             int32_t C[3];
         };
 
-        enum CalibrationConfigType { NONE, BMI160, MPU6050, MPU9250, ICM20948, BMI270 };
+        enum CalibrationConfigType { NONE, BMI160, MPU6050, MPU9250, ICM20948, BMI270, SFUSION };
 
         const char* calibrationConfigTypeToString(CalibrationConfigType type);
 
@@ -105,6 +122,7 @@ namespace SlimeVR {
             union {
                 BMI160CalibrationConfig bmi160;
                 BMI270CalibrationConfig bmi270;
+                SoftFusionCalibrationConfig sfusion;
                 MPU6050CalibrationConfig mpu6050;
                 MPU9250CalibrationConfig mpu9250;
                 ICM20948CalibrationConfig icm20948;

SlimeVR/SlimeVR-Tracker-ESP

src/configuration/CalibrationConfig.h
@@ -44,6 +44,23 @@ namespace SlimeVR {
             float temperature;
         };
 
+        struct SoftFusionCalibrationConfig {
+            // accelerometer offsets and correction matrix
+            float A_B[3];
+            float A_Ainv[3][3];
+
+            // magnetometer offsets and correction matrix
+            float M_B[3];
+            float M_Ainv[3][3];
+
+            // raw offsets, determined from gyro at rest
+            float G_off[3];
+
+            // calibration temperature for dynamic compensation
+            float temperature;
+        };
+
+
         struct MPU6050CalibrationConfig {
             // accelerometer offsets and correction matrix
             float A_B[3];
@@ -89,7 +106,7 @@ namespace SlimeVR {
             float G_off[3];
         };
 
-        enum CalibrationConfigType { NONE, BMI160, MPU6050, MPU9250, ICM20948, ICM42688 };
+        enum CalibrationConfigType { NONE, BMI160, MPU6050, MPU9250, ICM20948, ICM42688, SFUSION };
 
         const char* calibrationConfigTypeToString(CalibrationConfigType type);
 
@@ -98,6 +115,7 @@ namespace SlimeVR {
 
             union {
                 BMI160CalibrationConfig bmi160;
+                SoftFusionCalibrationConfig sfusion;
                 MPU6050CalibrationConfig mpu6050;
                 MPU9250CalibrationConfig mpu9250;
                 ICM20948CalibrationConfig icm20948;