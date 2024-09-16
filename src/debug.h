a6ea9c0c53dbd267cd6dc919b282530359d19ab9
kounocom <meia@kouno.xyz>
2024-09-16 18:58:40 +0300

    Default to non-blocking calibration

--- src/debug.h
+++ src/debug.h
@@ -34,7 +34,7 @@
 #define BIAS_DEBUG false // Printing BIAS Variables to serial (ICM20948 only)
 #define ENABLE_TAP false // monitor accel for (triple) tap events and send them. Uses more cpu, disable if problems. Server does nothing with value so disabled atm
 #define SEND_ACCELERATION true // send linear acceleration to the server
-
+#define USE_NONBLOCKING_CALIBRATION
 //Debug information
 
 #define LOG_LEVEL LOG_LEVEL_DEBUG
