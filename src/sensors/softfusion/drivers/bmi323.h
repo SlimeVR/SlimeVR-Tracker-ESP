7ded129e45a7a1647da3fd4c867120d04666d019
jojos38 <jojos38000@hotmail.com>
2024-09-19 20:49:14 +0200

    BMI323 sfusion ready

--- src/sensors/softfusion/drivers/bmi323.h
+++ src/sensors/softfusion/drivers/bmi323.h
@@ -35,42 +35,56 @@ namespace SlimeVR::Sensors::SoftFusion::Drivers
 template <typename I2CImpl>
 struct BMI323
 {
-    // Sensor Fusion required variables
-    static constexpr uint8_t Address = 0x68; // i2c address
+    /**
+     * Sensor Fusion required variables
+     */
+    static constexpr uint8_t Address = 0x68; // IMU's i2c address used to communicate
     static constexpr auto Name = "BMI323";
     static constexpr auto Type = ImuID::BMI323;
-
-    static constexpr float GyrTs = 1.0/50.0; // Delay between each gyro sample
-    static constexpr float AccTs = 1.0/50.0; // Dealy between each accel sample
+    
+    static constexpr float GyrTs = 1.0/200.0; // Delay between each gyro sample
+    static constexpr float AccTs = 1.0/200.0; // Dealy between each accel sample
     static constexpr float MagTs = 1.0/25.0; // Delay between each mag sample
 
-    // BMI323 specific constants
     static constexpr float GyroSensitivity = 32.768f;
     static constexpr float AccelSensitivity = 4096.0f;
     static constexpr float GScale = ((32768. / GyroSensitivity) / 32768.) * (PI / 180.0);
     static constexpr float AScale = CONST_EARTH_GRAVITY / AccelSensitivity;
 
     // FIFO variables
+    float latestTemperature = 0;
     static constexpr int8_t FifoFrameLength = BMI323_LIB::LENGTH_FIFO_ACCEL + BMI323_LIB::LENGTH_FIFO_GYRO + BMI323_LIB::LENGTH_TEMPERATURE;
-    uint8_t fifoData[I2C_BUFFER_LENGTH] = { 0 }; // 2048 is the maximum size of the FIFO
-    int16_t accelData[3] = { 0 };
-    int16_t gyroData[3] = { 0 };
-    float temperatureData = 0;
-    int16_t magData[3] = { 0 };
-
-    // FIFO Data validity bytes
     static const uint8_t ACCEL_VALID = 0x01;   // Bit 0 represents accelerometer data validity
     static const uint8_t GYRO_VALID = 0x02;    // Bit 1 represents gyroscope data validity
     static const uint8_t TEMP_VALID = 0x04;    // Bit 2 represents temperature data validity
 
+    // Intialization and variables
     I2CImpl i2c;
     SlimeVR::Logging::Logger &logger;
     BMI323_LIB bmi323Lib;
-    // BMI323(I2CImpl i2c, SlimeVR::Logging::Logger &logger): i2c(i2c), logger(logger), zxFactor(0) {}
     BMI323(I2CImpl i2c, SlimeVR::Logging::Logger &logger): i2c(i2c), logger(logger), bmi323Lib(&i2cRead, &i2cWrite, &delayUs, &i2c) {}
 
+    /**
+     * Calibration data for the basic static calibration
+     * Detected by sfusion and saved when values are put in
+     * Anything can be put in this struct
+     */
+    struct MotionlessCalibrationData
+    {
+        bool valid;
+
+        // accelerometer offsets and gain
+        uint16_t G_O[3];
+        uint8_t G_G[3];
+    };
 
     struct Regs {
+        /**
+         * The first value (reg) defines which register address the chip ID is located at
+         * The second value (value) defines the expected value
+         * This is used to make sure we have the correct IMU
+         * BUT... It doesn't work on this IMU so I bypassed it
+         */
         struct WhoAmI {
             static constexpr uint8_t reg = 0x00;
             static constexpr uint8_t value = 0x00; // 0x43
@@ -84,42 +98,44 @@ struct BMI323
 
     static int8_t i2cRead(uint8_t registerAddress, uint8_t *registerData, uint32_t length, void *interfacePointer)
     {
-        uint8_t address = 0x68;
-
-        // Read data from the sensor
-        Wire.beginTransmission(address);
+        Wire.beginTransmission(Address);
         Wire.write(registerAddress);
         Wire.endTransmission();
-        Wire.requestFrom(address, length);
+        Wire.requestFrom(Address, length);
         for (auto i = 0u; i < length; i++) {
             registerData[i] = Wire.read();
         }
-
         return 0;
-
-        /*I2CImpl i2c = *static_cast<I2CImpl*>(interfacePointer);
-        i2c.readBytes(registerAddress, length, registerData);
+        /*I2CImpl* i2c = static_cast<I2CImpl*>(interfacePointer);;
+        i2c->readBytes(registerAddress, length + 2, registerData);
         return 0;*/
     }
 
     static int8_t i2cWrite(uint8_t registerAddress, const uint8_t *registerData, uint32_t length, void *interfacePointer)
     {
-        uint8_t dev_addr = 0x68;
-
-        // Write data to the sensor
-        Wire.beginTransmission(dev_addr);
+        Wire.beginTransmission(Address);
         Wire.write(registerAddress);
         for (auto i = 0u; i < length; i++) {
             Wire.write(registerData[i]);
         }
         Wire.endTransmission();
-        
         return 0;
-
-        /*I2CImpl i2c = *static_cast<I2CImpl*>(interfacePointer);
-        i2c.writeBytes(registerAddress, length, const_cast<uint8_t*>(registerData));
+        /*I2CImpl* i2c = static_cast<I2CImpl*>(interfacePointer);
+        i2c->writeBytes(registerAddress, length + 2, const_cast<uint8_t*>(registerData));
         return 0;*/
     }
+
+    /**
+     * @brief Print calibration data
+     */
+    void printCalibrationData(MotionlessCalibrationData &calibrationData) {
+        if (!calibrationData.valid) {
+            logger.debug("No calibration data");
+        } else {
+            logger.debug("Gyro offset: %u, %u, %u", calibrationData.G_O[0], calibrationData.G_O[1], calibrationData.G_O[2]);
+            logger.debug("Gyro gain: %u, %u, %u", calibrationData.G_G[0], calibrationData.G_G[1], calibrationData.G_G[2]);
+        }
+   }
 
     /**
      * @brief Extracts the next frame from a FIFO frame
@@ -127,6 +143,7 @@ struct BMI323
      * be reset before the first call
     */
     static uint8_t extractFrame(uint8_t *data, uint8_t index, int16_t *accelData, int16_t *gyroData, float *tempData) {
+        // Set dataValidity to all valid
         uint8_t dataValidity = ACCEL_VALID | GYRO_VALID | TEMP_VALID;
         
         // Unpack accelerometer
@@ -146,15 +163,9 @@ struct BMI323
         if (isValid == BMI323_LIB::FIFO_GYRO_DUMMY_FRAME) {
             dataValidity &= ~GYRO_VALID;
         } else {
-            #if BMI323_USE_TEMP_CAL
-                gyroData[0] = isValid;
-                gyroData[1] = (int16_t)((data[gyroIndex + 3] << 8) | data[gyroIndex + 2]);
-                gyroData[2] = (int16_t)((data[gyroIndex + 5] << 8) | data[gyroIndex + 4]);
-            #else
-                gyroData[0] = (isValid);
-                gyroData[1] = ((int16_t)((data[gyroIndex + 3] << 8) | data[gyroIndex + 2]));
-                gyroData[2] = ((int16_t)((data[gyroIndex + 5] << 8) | data[gyroIndex + 4]));
-            #endif
+            gyroData[0] = (isValid);
+            gyroData[1] = ((int16_t)((data[gyroIndex + 3] << 8) | data[gyroIndex + 2]));
+            gyroData[2] = ((int16_t)((data[gyroIndex + 5] << 8) | data[gyroIndex + 4]));
         }
 
         // Unpack temperature data
@@ -169,7 +180,7 @@ struct BMI323
         return dataValidity;
     }
 
-    bool initialize()
+    bool initialize(MotionlessCalibrationData &calibrationData)
     {
         logger.info("jojos38 BMI323 firmware V1.4");
         
@@ -187,24 +198,23 @@ struct BMI323
         logger.info("Chip ID 0x%x", bmi323Lib.chipId);
 
         // Apply the calibration data
-        /*if (m_calibration.G_G[0] != 0 || m_calibration.G_G[1] != 0 || m_calibration.G_G[2] != 0 || m_calibration.G_O[0] != 0 || m_calibration.G_O[1] != 0 || m_calibration.G_O[2] != 0) {
-            m_Logger.info("Calibration data found");
-            printCalibrationData();
-            result = bmi323.setGyroOffsetGain(m_calibration.G_O, m_calibration.G_G);
-            if (result == BMI323::SUCCESS) {
-                m_Logger.info("BMI323 Calibration data applied");
+        if (calibrationData.valid) {
+            logger.info("Calibration data found");
+            printCalibrationData(calibrationData);
+            result = bmi323Lib.setGyroOffsetGain(calibrationData.G_O, calibrationData.G_G);
+            if (result == BMI323_LIB::SUCCESS) {
+                logger.info("BMI323 Calibration data applied");
             } else {
-                m_Logger.error("BMI323 Calibration data apply failed");
-                error = true;
+                logger.error("BMI323 Calibration data apply failed");
+                return false;
             }
         } else {
-            m_Logger.warn("No calibration data found, please calibrate the sensor it only takes a few seconds");
-            calibrate = true;
-        }*/
+            logger.warn("No calibration data found, please calibrate the sensor");
+        }
 
         // Set gyroscope configuration
         result = bmi323Lib.setGyroConfig(
-            BMI323_LIB::GYRO_ODR_50HZ,
+            BMI323_LIB::GYRO_ODR_200HZ,
             BMI323_LIB::GYRO_BANDWIDTH_ODR_HALF,
             BMI323_LIB::GYRO_MODE_HIGH_PERF,
             BMI323_LIB::GYRO_RANGE_1000DPS,
@@ -219,7 +229,7 @@ struct BMI323
 
         // Set accelerometer configuration
         result = bmi323Lib.setAccelConfig(
-            BMI323_LIB::ACCEL_ODR_50HZ,
+            BMI323_LIB::ACCEL_ODR_200HZ,
             BMI323_LIB::ACCEL_BANDWIDTH_ODR_HALF,
             BMI323_LIB::ACCEL_MODE_HIGH_PERF,
             BMI323_LIB::ACCEL_RANGE_8G,
@@ -241,84 +251,58 @@ struct BMI323
             return false;
         }
 
-        #if BMI323_USE_BMM350
-            // BMM350
-            bmm350Address = (this->address == BMI323::ADDRESS_I2C_PRIMARY) ? BMM350::ADDRESS_I2C_PRIMARY : BMM350::ADDRESS_I2C_SECONDARY;
-            result = bmm350.initI2C();
-            if (result == BMM350::SUCCESS) {
-                m_Logger.info(String("BMM350 initialized on address 0x" + String(bmm350Address, HEX)).c_str());
-            } else {
-                m_Logger.info("BMM350 initialization failed");
-                error = true;
-            }
-
-            // Configure the sensor
-            result = bmm350.seOdrAvg(BMM350::ODR_25HZ, BMM350::AVG_4);
-            if (result == BMM350::SUCCESS) {
-                m_Logger.info("BMM350 configured");
-            } else {
-                m_Logger.error("BMM350 configuration failed");
-                error = true;
-            }
-
-            // Disable interrupt
-            result = bmm350.setInterruptEnabled(BMM350::DISABLE);
-            if (result == BMM350::SUCCESS) {
-                m_Logger.info("BMM350 interrupt disabled");
-            } else {
-                m_Logger.error("BMM350 interrupt disable failed");
-                error = true;
-            }
-
-            // Set power mode
-            result = bmm350.setPowermode(BMM350::NORMAL_MODE);
-            if (result == BMM350::SUCCESS) {
-                m_Logger.info("BMM350 power mode set");
-            } else {
-                m_Logger.error("BMM350 power mode set failed");
-                error = true;
-            }
-        #endif
-
-        #if BMI323_USE_TEMP_CAL
-            // allocate temperature memory after calibration because OOM
-            gyroTempCalibrator = new GyroTemperatureCalibrator(
-                SlimeVR::Configuration::CalibrationConfigType::BMI323,
-                sensorId,
-                GyroSensitivity,
-                (uint32_t)(0.2f / (1.0f / 200.0f))
-            );
-
-            gyroTempCalibrator->loadConfig(GyroSensitivity);
-            if (gyroTempCalibrator->config.hasCoeffs) {
-                gyroTempCalibrator->approximateOffset(m_calibration.temperature, GOxyzStaticTempCompensated);
-            }
-        #endif
-
-        #if BMI323_USE_SENSCAL
-            String localDevice = WiFi.macAddress();
-            for (auto const& offsets : sensitivityOffsets) {
-                if (!localDevice.equals(offsets.mac)) continue;
-                if (offsets.sensorId != sensorId) continue;
-                #define BMI323_CALCULATE_SENSITIVTY_MUL(degrees) (1.0 / (1.0 - ((degrees)/(360.0 * offsets.spins))))
-                gScaleX = GScale * BMI323_CALCULATE_SENSITIVTY_MUL(offsets.x);
-                gScaleY = GScale * BMI323_CALCULATE_SENSITIVTY_MUL(offsets.y);
-                gScaleZ = GScale * BMI323_CALCULATE_SENSITIVTY_MUL(offsets.z);
-                m_Logger.debug("Custom sensitivity offset enabled: %s %s",
-                    offsets.mac,
-                    offsets.sensorId == SENSORID_PRIMARY ? "primary" : "aux"
-                );
-            }
-        #endif
-
         logger.info("BMI323 initialized");
 
         return true;
     }
 
-    void motionlessCalibration()
+    void motionlessCalibration(MotionlessCalibrationData &calibrationData)
     {
+        // Calibrate sensitivity (ALWAYS FIRST)
+        logger.info("Calibrating gyroscope sensitivity in 5 seconds... Please do not move the device");
+        delay(5000);
+        ledManager.on();
+        struct BMI323_LIB::SelfCalibResult calibSensitivityResult;
+        uint8_t result = bmi323Lib.performGyroCalibration(BMI323_LIB::CALIBRATION_SENSITIVITY, BMI323_LIB::CALIBRATION_APPLY_TRUE, &calibSensitivityResult);
+        if (result == BMI323_LIB::SUCCESS) {
+            logger.info("Gyroscope sensitivity calibration succeed");
+        } else {
+            logger.error("Gyroscope sensitivity calibration failed");
+            return;
+        }
 
+        // Calibrate offset
+        logger.info("Calibrating gyroscope offset... Please do not move the device");
+        struct BMI323_LIB::SelfCalibResult calibOffsetResult;
+        result = bmi323Lib.performGyroCalibration(BMI323_LIB::CALIBRATION_OFFSET, BMI323_LIB::CALIBRATION_APPLY_TRUE, &calibOffsetResult);
+        if (result == BMI323_LIB::SUCCESS) {
+            logger.info("Gyroscope offset calibration succeed");
+        } else {
+            logger.error("Gyroscope offset calibration failed");
+            return;
+        }
+
+        // Save results
+        uint16_t offset[3];
+        uint8_t gain[3];
+        bmi323Lib.getGyroOffsetGain(offset, gain);
+
+        // Save the calibration data
+        logger.info("Saving calibration data");
+        calibrationData.G_O[0] = offset[0];
+        calibrationData.G_O[1] = offset[1];
+        calibrationData.G_O[2] = offset[2];
+
+        calibrationData.G_G[0] = gain[0];
+        calibrationData.G_G[1] = gain[1];
+        calibrationData.G_G[2] = gain[2];
+
+        calibrationData.valid = true;
+
+        printCalibrationData(calibrationData);
+
+        logger.info("Motionless calibration done");
+        ledManager.off();
     }
 
     template <typename AccelCall, typename GyroCall>
@@ -328,20 +312,30 @@ struct BMI323
         bmi323Lib.getFifoLength(&availableFifoLength);
 
         // Check for FIFO overflow
-        if (availableFifoLength >= 127) {
+        /*if (availableFifoLength >= 127) {
             logger.error("FIFO OVERFLOW");
-        }
+        }*/
 
         // If there is enough data in the FIFO to get at least a single frame
-        if (availableFifoLength >= FifoFrameLength) {        
+        if (availableFifoLength >= FifoFrameLength) {  
+
+            // Variable to store FIFO data
+            uint8_t fifoData[I2C_BUFFER_LENGTH] = { 0 };
+
             // Make sure the length that we read is a multiple of the frame length
             uint16_t fifoReadLength = availableFifoLength = std::min(availableFifoLength, static_cast<uint16_t>(I2C_BUFFER_LENGTH - BMI323_LIB::DUMMY_BYTE)) / FifoFrameLength * FifoFrameLength + BMI323_LIB::DUMMY_BYTE;
+            
+            // Read the FIFO data
             int8_t result = bmi323Lib.readFifoData(fifoData, fifoReadLength);
+
             if (result == BMI323_LIB::SUCCESS) {
-                // Feed sensor fusion
+                // Count the number of frames in the FIFO
                 const uint8_t frameCount = fifoReadLength / FifoFrameLength;
+                // Unpack each frame one by one
                 for (int i = 0; i < frameCount; i++) {
                     float temperature = 0;
+                    int16_t accelData[3] = { 0 };
+                    int16_t gyroData[3] = { 0 };
                     uint8_t dataValidity = extractFrame(fifoData, i, accelData, gyroData, &temperature);
 
                     // If the accel data is valid
@@ -356,7 +350,7 @@ struct BMI323
 
                     // If the temp data is valid
                     if (dataValidity & TEMP_VALID) {
-                        temperatureData = temperature;
+                        latestTemperature = temperature;
                     }
                 }
             } else {
@@ -367,7 +361,7 @@ struct BMI323
 
     float getDirectTemp() const
     {
-        return temperatureData;
+        return latestTemperature;
     }
 
 };
