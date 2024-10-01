44e2aaf75d840d5fedae02b3d2f308eeb1440406
kounocom <meia@kouno.xyz>
2024-10-01 21:06:15 +0300

    This works better

--- lib/vqf/vqf.h
+++ lib/vqf/vqf.h
@@ -125,7 +125,7 @@ struct VQFParams
      *
      * Default value: 0.1 °/s
      */
-    vqf_real_t biasSigmaMotion = 0.1;
+    vqf_real_t biasSigmaMotion = 0.1125;
     /**
      * @brief Forgetting factor for unobservable bias in vertical direction during motion.
      *
@@ -153,7 +153,7 @@ struct VQFParams
      *
      * Default value: 1.5 s
      */
-    vqf_real_t restMinT = 1.0;
+    vqf_real_t restMinT = 1.5;
     /**
      * @brief Time constant for the low-pass filter used in rest detection (in seconds).
      *
@@ -162,7 +162,7 @@ struct VQFParams
      *
      * Default value: 0.5 s
      */
-    vqf_real_t restFilterTau = 0.25;
+    vqf_real_t restFilterTau = 0.025;
     /**
      * @brief Angular velocity threshold for rest detection (in °/s).
      *
