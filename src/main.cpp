d8b51aaeb4bbe75c9ec52eaa73c3fdb7f1d1c579
Meia <71262281+kounocom@users.noreply.github.com>
2024-12-11 17:02:46 +0200

    Remove ESP32-C3 startup delay (#329)

--- src/main.cpp
+++ src/main.cpp
@@ -53,11 +53,6 @@ void setup() {
 	Serial.begin(serialBaudRate);
 	globalTimer = timer_create_default();
 
-#ifdef ESP32C3
-	// Wait for the Computer to be able to connect.
-	delay(2000);
-#endif
-
 	Serial.println();
 	Serial.println();
 	Serial.println();
