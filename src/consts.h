b873ba66dd39172f36ccdc1821151d6fc85a8814
Ilia Ki <ki9ilia@gmail.com>
2025-03-30 23:23:09 +0700

    Add Gestures Boards (#413)
    * Add Gestures Boards
    
    * Update src/consts.h
    
    Co-authored-by: gorbit99 <gorbitgames@gmail.com>
    
    ---------
    
    Co-authored-by: gorbit99 <gorbitgames@gmail.com>

--- src/consts.h
+++ src/consts.h
@@ -92,6 +92,7 @@ enum class SensorTypeID : uint8_t {
 #define BOARD_HARITORA 18  // Used by Haritora/SlimeTora
 #define BOARD_ES32C6DEVKITC1 19
 #define BOARD_GLOVE_IMU_SLIMEVR_DEV 20  // IMU Glove
+#define BOARD_GESTURES 21  // Used by Gestures
 #define BOARD_DEV_RESERVED 250  // Reserved, should not be used in any release firmware
 
 #define BAT_EXTERNAL 1
