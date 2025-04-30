6464004c2c3880f3b86806f8b35f07433129e72f
gorbit99 <gorbitgames@gmail.com>
2025-04-30 18:59:06 +0200

    Undo defines changes

--- src/defines.h
+++ src/defines.h
@@ -236,16 +236,15 @@ PIN_IMU_SDA, PRIMARY_IMU_OPTIONAL, BMI160_QMC_REMAP) \
 #define BATTERY_MONITOR BAT_EXTERNAL
 
 // BAT_EXTERNAL definition override
-// D1 Mini boards with ESP8266 have internal resistors. For these boards you
-// only have to adjust BATTERY_SHIELD_RESISTANCE. For other boards you can now
-// adjust the other resistor values. The diagram looks like this:
-//   (Battery)--- [BATTERY_SHIELD_RESISTANCE] ---(INPUT_BOARD)---
-//   [BATTERY_SHIELD_R2]
+// D1 Mini boards with ESP8266 have internal resistors. For these boards you only have
+// to adjust BATTERY_SHIELD_RESISTANCE. For other boards you can now adjust the other
+// resistor values. The diagram looks like this:
+//   (Battery)--- [BATTERY_SHIELD_RESISTANCE] ---(INPUT_BOARD)---  [BATTERY_SHIELD_R2]
 //   ---(ESP32_INPUT)--- [BATTERY_SHIELD_R1] --- (GND)
-// #define BATTERY_SHIELD_RESISTANCE 180 //130k BatteryShield, 180k SlimeVR or
-// fill in external resistor value in kOhm #define BATTERY_SHIELD_R1 100 //
-// Board voltage divider resistor Ain to GND in kOhm #define BATTERY_SHIELD_R2
-// 220 // Board voltage divider resistor Ain to INPUT_BOARD in kOhm
+// #define BATTERY_SHIELD_RESISTANCE 180 //130k BatteryShield, 180k SlimeVR or fill in
+// external resistor value in kOhm #define BATTERY_SHIELD_R1 100 // Board voltage
+// divider resistor Ain to GND in kOhm #define BATTERY_SHIELD_R2 220 // Board voltage
+// divider resistor Ain to INPUT_BOARD in kOhm
 
 // LED configuration:
 // Configuration Priority 1 = Highest:
@@ -377,8 +376,8 @@ PIN_IMU_SDA, PRIMARY_IMU_OPTIONAL, BMI160_QMC_REMAP) \
 #define PIN_IMU_INT_2 7
 #define PIN_BATTERY_LEVEL 3
 #define LED_PIN \
-	LED_OFF  // RGB LED Protocol would need to be implementetet did not brother
-			 // for the test, because the board ideal for tracker ifself
+	LED_OFF  // RGB LED Protocol would need to be implementetet did not brother for the
+			 // test, because the board ideal for tracker ifself
 //  #define LED_INVERTED false
 #elif BOARD == BOARD_WEMOSWROOM02
 #define PIN_IMU_SDA 2
