6818bb52c3697508d0e23171b3bc5b22e644f841
loucass003 <loucass003@gmail.com>
2025-10-22 15:17:49 +0200

    Basic Nix Flake for dev env

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "PlatformIO development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # PlatformIO
+            platformio
+            platformio-core
+
+            # Python for PlatformIO
+            python3
+            python3Packages.pip
+            python3Packages.virtualenv
+
+            # Build tools
+            gcc
+            gnumake
+            cmake
+
+            # Serial communication
+            picocom
+            minicom
+
+            # USB access (for programming devices)
+            libusb1
+            pkg-config
+          ];
+
+          shellHook = ''
+            # Set PlatformIO core directory to project-local directory
+            export PLATFORMIO_CORE_DIR=$PWD/.nix-platformio
+
+            echo "PlatformIO development environment loaded"
+            echo "PlatformIO version: $(pio --version)"
+            echo ""
+            echo "Available commands:"
+            echo "  pio init      - Initialize a new PlatformIO project"
+            echo "  pio run       - Build the project"
+            echo "  pio run -t upload  - Upload to device"
+            echo "  pio device monitor - Open serial monitor"
+            echo ""
+
+            # Set up udev rules for device access (may need to be done manually)
+            if [ ! -f /etc/udev/rules.d/99-platformio-udev.rules ]; then
+              echo "Note: For USB device access, you may need to install udev rules:"
+              echo "  curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core/master/scripts/99-platformio-udev.rules | sudo tee /etc/udev/rules.d/99-platformio-udev.rules"
+              echo "  sudo udevadm control --reload-rules"
+              echo "  sudo udevadm trigger"
+              echo ""
+            fi
+          '';
+        };
+      }
+    );
+}
