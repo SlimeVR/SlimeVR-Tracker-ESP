e00379738bc567aa1466345cb6f138060ef6df8e
unlogisch04 <98281608+unlogisch04@users.noreply.github.com>
2026-09-07 15:11:09 +0200

    remove complex timing for temp Intervall (#547)
    This logic has a problem with the start on the tracker or longer not called functions, it will try to resend each missed packet in a short of time and make a burst of temp packets on a tracker reboot.

--- src/sensors/softfusion/softfusionsensor.h
+++ src/sensors/softfusion/softfusionsensor.h
@@ -95,7 +95,7 @@ class SoftFusionSensor : public Sensor {
 		constexpr uint32_t sendInterval = 1.0f / maxSendRateHz * 1e6;
 		uint32_t elapsed = now - m_lastTemperaturePacketSent;
 		if (elapsed >= sendInterval) {
-			m_lastTemperaturePacketSent = now - (elapsed - sendInterval);
+			m_lastTemperaturePacketSent = now;
 			networkConnection.sendTemperature(sensorId, lastReadTemperature);
 		}
 	}
